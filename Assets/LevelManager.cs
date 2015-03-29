9e666634ffd495825ff1f69d03305302c542740b
lukasz-skibski <lukasz.skibski@gmail.com>
2015-03-29 23:34:04 +0200

    uproszczony level collider

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -8,12 +8,12 @@ public class LevelManager : MonoBehaviour {
 	public int initialBalls;
 
 	private ClickHandler clickHandler;
-	Vector3 myWorld;
-	Transform referenceBall;
+	private Vector3 myWorld;
+	private Transform referenceBall;
 	private List<BallType> currentBallTypes;
 	private BallType backgroundType;
 	
-	BallManager ballManager;
+	private BallManager ballManager;
 
 	// Use this for initialization
 	void Start () {
@@ -74,62 +74,18 @@ public class LevelManager : MonoBehaviour {
 
 	void SetupWorldSize()
 	{
-		Vector3 myScreen = new Vector3(Screen.width,Screen.height,0);
+		Vector3 myScreen = new Vector3(Screen.width, Screen.height, 0);
 		myWorld = Camera.main.ScreenToWorldPoint(myScreen);
+		background.transform.localScale = new Vector3(myWorld.x*2, myWorld.y*2, 1.0f);
 	}
 
 	void CreateBorders()
 	{
-		float verticalHeight = myWorld.y * 2;
-		float horizontalHeight = myWorld.x * 2;
-		float width = 1;
-
-		Transform border = ((GameObject)Resources.Load("Border")).transform;
-
-		Vector3 leftPos = new Vector3 (-myWorld.x - width/2, 0, 0);
-		Vector3 rightPos = new Vector3 (myWorld.x + width/2, 0, 0);
-		Vector3 upPos = new Vector3 (0, myWorld.y + width/2, 0);
-		Vector3 downPos = new Vector3 (0, -myWorld.y - width/2, 0);
-
-		Transform left = (Transform)Instantiate (border, leftPos, Quaternion.identity);
-		Transform right = (Transform)Instantiate (border, rightPos, Quaternion.identity);
-		Transform up = (Transform)Instantiate (border, upPos, Quaternion.identity);
-		Transform down = (Transform)Instantiate (border, downPos, Quaternion.identity);
-
-		Vector3 verticalSize = new Vector3 (width, verticalHeight, 1);
-		Vector3 horizontalSize = new Vector3 (width, horizontalHeight, 1);
-
-		left.localScale = verticalSize;
-		right.localScale = verticalSize;
-		up.localScale = horizontalSize;
-		down.localScale = horizontalSize;
-
-		up.Rotate(new Vector3(0,0,90));
-		down.Rotate(new Vector3(0,0,90));
-
-
-
-
-
-
-		//left.renderer.bounds.size = verticalSize;
-
+		Vector2[] points = new Vector2[] { new Vector2 (-myWorld.x, -myWorld.y),
+			new Vector2 (myWorld.x, -myWorld.y),
+			new Vector2 (myWorld.x, myWorld.y),
+			new Vector2 (-myWorld.x, myWorld.y),
+			new Vector2 (-myWorld.x, -myWorld.y)};
+		gameObject.GetComponent<EdgeCollider2D>().points = points;
 	}
-
-//	public Color BackgroundColor
-//	{
-//		get 
-//		{
-//			return background.renderer.material.color;
-//		}
-//
-//		set
-//		{
-//			background.renderer.material.color = value;
-//		}
-//	}
-//	public static Color getBackgroundColor()
-//	{
-//		return background.renderer.material.color;
-//	}
 }
