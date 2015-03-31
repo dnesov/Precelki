ae789ad87054fe809b7cb42dcf97254d186fe3de
lukasz-skibski <lukasz.skibski@gmail.com>
2015-03-31 20:58:01 +0200

    inne menu, spowolnienie kulek

---
+++ Assets/MenuAnimation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAnimation : MonoBehaviour {
+
+	public GameObject blueFace;
+	public GameObject greenFace;
+	public GameObject orangeFace;
+	public GameObject redFace;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		blueFace.transform.localScale = new Vector3(0.3f, 0.3f, 0) * lerp(1.0f);
+		greenFace.transform.localScale = new Vector3(0.3f, 0.3f, 0) * lerp(0.0f);
+		orangeFace.transform.localScale = new Vector3(0.3f, 0.3f, 0) * lerp(1.0f);
+		redFace.transform.localScale = new Vector3(0.3f, 0.3f, 0) * lerp(0.0f);
+	}
+	
+	private float lerp(float offset) {
+		return (Mathf.PingPong(Time.time + offset, 1.0f)) * 0.7f + 0.5f;
+	}
+}
